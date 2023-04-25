47e466afa7f4f38a2b7dcef2821695b61b95a036
bakryH <bakryha2@illinois.edu>
2023-04-24 21:15:58 -0500

    Some progress

---
+++ Tailored Itinerary program/itin_methods.cpp
@@ -0,0 +1,34 @@
+#include "itin_methods.h"
+#include <string>
+
+using namespace std;
+
+itinerary::itinerary (){
+
+
+}
+
+itinerary::itinerary (string file){
+
+
+}
+
+void itinerary::view_itinerary(){
+
+
+}
+
+void itinerary::purchase_itinerary(){
+
+
+}
+
+void refund_itinerary(){
+
+
+}
+
+void listfiles(string folder){
+
+    
+}
--- Tailored Itinerary program/itin_methods.h
+++ Tailored Itinerary program/itin_methods.h
@@ -8,10 +8,10 @@ using namespace std;
 class itinerary {
     public:
     itinerary (); // creates new itinerary
-    itinerary (string file); // initializes an exisitng itinerary to read or purchase. (calls either function)
-    void view_itinerary();
-    void purchase_itinerary();
-    void refund_itinerary();
+    itinerary (string file); // initializes an exisiting itinerary to read or purchase. (calls either function)
+    void view_itinerary(); // function that prints out an itinerary given it has a file_name
+    void purchase_itinerary(); // function that emulates a purchase of an itinerary given a file_name
+    void refund_itinerary(); // function emulates a refund process given a file_name
     string file_name;
     private:
     int flag_to_verify_purchase;
